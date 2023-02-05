ee9a2be7ccfaca49eab933d3ec32e49210674988
t-skgm <skgm0301@gmail.com>
2023-02-05 11:34:33 +0900

    add preact integ

---
+++ src/components/PreactCompTest.tsx
@@ -0,0 +1,11 @@
+import type { FunctionComponent } from "preact";
+import { useState } from "preact/hooks";
+
+export const PreactCompTest: FunctionComponent = ({ children }) => {
+  const [count, setCount] = useState(0);
+  return (
+    <div onClick={() => setCount((p) => p + 1)}>
+      {children}, you clicked x{count}
+    </div>
+  );
+};
