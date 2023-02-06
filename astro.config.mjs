5e4ee24b3ee27df96fff019f0f0e505041a3187e
t-skgm <skgm0301@gmail.com>
2023-02-06 10:23:30 +0900

    header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config'
 import preact from '@astrojs/preact'
 import tailwind from '@astrojs/tailwind'
+import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
-  markdown: { drafts: true },
-  integrations: [preact(), tailwind()],
+  markdown: {
+    drafts: true,
+    gfm: true
+  },
+  integrations: [preact(), tailwind(), mdx()],
   vite: {
     ssr: {
       noExternal: ['path-to-regexp', 'tinacms']
