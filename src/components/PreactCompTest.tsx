c912dcc02dcb262c4ec7386dc706aed342d823c6
t-skgm <skgm0301@gmail.com>
2023-02-20 15:29:10 +0900

    fix comps

--- src/components/PreactCompTest.tsx
+++ src/components/PreactCompTest.tsx
@@ -4,7 +4,11 @@ import { useState } from 'preact/hooks'
 export const PreactCompTest: FunctionComponent = ({ children }) => {
   const [count, setCount] = useState(0)
   return (
-    <div onClick={() => setCount((p) => p + 1)}>
+    <div
+      onClick={() => {
+        setCount((p) => p + 1)
+      }}
+    >
       {children}, you clicked x{count}
     </div>
   )
