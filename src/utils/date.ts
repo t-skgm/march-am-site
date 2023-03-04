8674d2b82aa6ef58c1940d446ec5e02009853b90
t-skgm <skgm0301@gmail.com>
2023-03-05 08:41:54 +0900

    use tokyo local time

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,3 +1,10 @@
 import dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc'
+import timezone from 'dayjs/plugin/timezone'
 
-export const formatDate = (date: dayjs.ConfigType) => dayjs(date).format('YYYY.MM.DD')
+dayjs.extend(utc)
+dayjs.extend(timezone)
+
+export const dayJp = (date: dayjs.ConfigType) => dayjs.tz(date, 'Asia/Tokyo')
+
+export const formatDateJp = (date: dayjs.ConfigType) => dayJp(date).format('YYYY.MM.DD')
