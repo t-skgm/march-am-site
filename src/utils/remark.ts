6b11becf1e6c99c8565ee637b42056c327697d58
t-skgm <skgm0301@gmail.com>
2023-05-28 18:13:44 +0900

    enable remark slug

--- src/utils/remark.ts
+++ src/utils/remark.ts
@@ -2,6 +2,7 @@ import { unified } from 'unified'
 import remarkParse from 'remark-parse'
 import remarkRehype from 'remark-rehype'
 import remarkToc from 'remark-toc'
+import remarkSlug from 'remark-slug'
 import rehypeStringify from 'rehype-stringify'
 import type { Options as OptionsToc } from 'mdast-util-toc/lib'
 import type { Options as OptionsToHast } from 'mdast-util-to-hast/lib'
@@ -11,6 +12,8 @@ import type { Options as OptionsToHtml } from 'hast-util-to-html'
 
 export const remark = unified()
   .use(remarkParse)
+  // 見出しに ID を自動付与する
+  .use(remarkSlug)
   .use(remarkToc, {
     heading: '目次',
     tight: true
