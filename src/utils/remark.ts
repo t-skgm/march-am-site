eb263517e1b3cc00052857c690ff1b70f2071b08
t-skgm <skgm0301@gmail.com>
2023-05-28 18:10:19 +0900

    enable remark toc

--- src/utils/remark.ts
+++ src/utils/remark.ts
@@ -1,12 +1,22 @@
 import { unified } from 'unified'
 import remarkParse from 'remark-parse'
 import remarkRehype from 'remark-rehype'
+import remarkToc from 'remark-toc'
 import rehypeStringify from 'rehype-stringify'
+import type { Options as OptionsToc } from 'mdast-util-toc/lib'
+import type { Options as OptionsToHast } from 'mdast-util-to-hast/lib'
+import type { Options as OptionsToHtml } from 'hast-util-to-html'
+
+/* eslint-disable @typescript-eslint/consistent-type-assertions */
 
 export const remark = unified()
   .use(remarkParse)
-  .use(remarkRehype, { allowDangerousHtml: true })
-  .use(rehypeStringify, { allowDangerousHtml: true })
+  .use(remarkToc, {
+    heading: '目次',
+    tight: true
+  } as OptionsToc)
+  .use(remarkRehype, { allowDangerousHtml: true } as OptionsToHast)
+  .use(rehypeStringify, { allowDangerousHtml: true } as OptionsToHtml)
 
 export const processMarkdown = async (mdStr: string): Promise<string> =>
   await remark.process(mdStr).then((vfile) => String(vfile))
