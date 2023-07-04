0af438659316025e434594592cb22bbba180c918
t-skgm <skgm0301@gmail.com>
2023-07-04 15:12:18 +0900

    不要なファイル削除

--- src/utils/getTokenNode.ts
+++ src/utils/getTokenNode.ts
@@ -1,8 +1,5 @@
 import { createHmac } from 'node:crypto'
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-// const secret = process.env.ENCRYTPTION_SECRET!
-
 export const getToken = (secret: string, value: Record<string, unknown>) => {
   const hmac = createHmac('sha256', secret)
   hmac.update(JSON.stringify(value))
